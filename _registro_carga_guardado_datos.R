03f49b653abb31d6471a9ef9d2406ccdb9a1b822
Andrés González Santa Cruz <gonzalez.santacruz.andres@gmail.com>
2023-05-12 08:51:04 -0400

    corregí añadiendo año por pueblo originario, lo hice en Paso33 y Paso34

---
+++ _registro_carga_guardado_datos.R
@@ -0,0 +1,3 @@
+#Paso2  -sin distal outcome- save.image("data2_lca2.RData") #save.image("data2_lca2.RData")
+#Paso22 -con distal outcome- load("data2_lca2_2023_04_26.RData") #save.image("data2_lca2_adj.RData")
+#Paso 3 -ver resultados de ambos- load("data2_lca2_adj_2023_04_27.RData") #save.image("data2_lca3.RData") #MEJOR: data2_lca3_adj_2023_04_28.RData
